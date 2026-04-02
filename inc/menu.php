d971e2ea9d0bce476c463e55b2e0aff2cb87f465
Hany Darwish <hany.freestyle4u@gmail.com>
2026-04-03 00:51:36 +0200

    Update

---
+++ inc/menu.php
@@ -0,0 +1,18 @@
+<header class="top-header">
+    <div class="container navbar">
+        <div class="logo">
+            <a href="index.php"> <img src="img/logo.png"></a>
+        </div>
+
+        <ul class="menu">
+            <li><a href="index.php">Home</a></li>
+            <li><a href="#">About Us</a></li>
+            <li><a href="#">Web Hosting</a></li>
+            <li><a href="#">Online Store</a></li>
+            <li><a href="#">Online Resume</a></li>
+            <li><a href="#">Networking</a></li>
+            <li><a href="#">Surveillance System</a></li>
+            <li><a class="activex" href="contact-us.php">Contact Us</a></li>
+        </ul>
+    </div>
+</header>
